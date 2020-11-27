02fbf915aa8708edc8d0b92bb34cc2f1b155576c
Lothar Rubusch <l.rubusch@gmx.ch>
2020-11-27 15:11:04 +0100

    scripts: update build script to init an external zephyrproject folder

--- docker/build.sh
+++ docker/build.sh
@@ -1,19 +1,23 @@
 #!/bin/bash -e
 
-export MY_HOME=/home/$(whoami)
-export BUILDDIR=${MY_HOME}/zephyrproject
+export MY_HOME="/home/$(whoami)"
+export BUILDDIR="${MY_HOME}/zephyrproject"
+export ZEPHYR_BRANCH="v2.4-branch"
+
 
 ## TODO this will cost performance, improve!!!      
-chown ${USER}:${USER} -R ${BUILDDIR}
+sudo chown ${USER}:${USER} -R ${BUILDDIR}
 
 cd ${MY_HOME}
-west init /home/$USER/zephyrproject
+west init ${MY_HOME}/zephyrproject
 
 cd ${BUILDDIR}
 west update
 west zephyr-export
 pip3 install --user -r ${BUILDDIR}/zephyr/scripts/requirements.txt
-echo "TODO"                 
+
+cd ${BUILDDIR}/zephyr
+git checkout ${ZEPHYR_BRANCH}
 
 echo "READY."
 echo
